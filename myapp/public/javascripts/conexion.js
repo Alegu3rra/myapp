fdb8c301dd978a841bfd7f5badfd4f5730c27b08
Alejandra <alegu3rra@gmail.com>
2020-08-02 23:38:02 -0500

    CRUD API and MYSQL

--- myapp/public/javascripts/conexion.js
+++ myapp/public/javascripts/conexion.js
@@ -1,15 +1,21 @@
-var mysql      = require('mysql');
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+
+
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'dbuser',
-  password : 's3kreee7'
+    host: 'localhost',
+    user: 'root',
+    password: '8Conejos?',
+    database: 'prueba_node'
 });
 
 connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].solution);
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
 });
 
 connection.end();
+//end mysql
